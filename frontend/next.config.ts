d6df7c99c7a6da6eeaa57e8757aaab182df7b26a
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-09-05 16:51:44 +0600

    fix: nextconfig fir deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://kanban-backend-eta.vercel.app/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
